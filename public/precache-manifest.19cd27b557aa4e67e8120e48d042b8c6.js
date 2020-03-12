1aaa88ef48d4b51d137c3b51730e56ac1eeec4f0
cevent <1540001771@qq.com>
2020-03-12 15:58:41 +0800

    react nginx app

---
+++ public/precache-manifest.19cd27b557aa4e67e8120e48d042b8c6.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e77d93c15369dad390463c2425f6e42",
+    "url": "/index.html"
+  },
+  {
+    "revision": "41ff8a3d9beaa394193a",
+    "url": "/static/css/2.e43b3631.chunk.css"
+  },
+  {
+    "revision": "edd33bbccc04b2c4ee4c",
+    "url": "/static/css/main.490f9d2c.chunk.css"
+  },
+  {
+    "revision": "41ff8a3d9beaa394193a",
+    "url": "/static/js/2.59dc0cd2.chunk.js"
+  },
+  {
+    "revision": "7413fe6b6adea975ad65bf5d3220c7bf",
+    "url": "/static/js/2.59dc0cd2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "edd33bbccc04b2c4ee4c",
+    "url": "/static/js/main.5152a28b.chunk.js"
+  },
+  {
+    "revision": "e573d7b9d3cb3b941eb2",
+    "url": "/static/js/runtime-main.232daed9.js"
+  },
+  {
+    "revision": "9161e238b798c444b732dbbb7420a09e",
+    "url": "/static/media/404.9161e238.png"
+  },
+  {
+    "revision": "ab45504baf89be7c7792b4426c4a6594",
+    "url": "/static/media/bg.ab45504b.jpg"
+  },
+  {
+    "revision": "f071cb628a7f059da0efb805c2a4dc89",
+    "url": "/static/media/亚盟.f071cb62.png"
+  }
+]);
